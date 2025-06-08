f3e0a90c2a808a1305de9ac2b7808c414b455ad3
Abel Varghese <abelshinevarghese@gmail.com>
2025-06-08 23:53:06 +0400

    fix: Added cc_deck rest

--- cc_function_list.py
+++ cc_function_list.py
@@ -22,22 +22,22 @@ chance_deck = {
 CHANCE_GOJF_LOCK = False
 
 cc_deck = {
-    # "Advance to Go (Collect $200)": ["move", 0],
-    # "Bank error in your favour. Collect $200": ["gain", 200],
-    # "Doctor's fees. Pay $50": ['loss', 50],
-    # "From sale of stock you get $50": ['gain', 50],
+    "Advance to Go (Collect $200)": ["move", 0],
+    "Bank error in your favour. Collect $200": ["gain", 200],
+    "Doctor's fees. Pay $50": ['loss', 50],
+    "From sale of stock you get $50": ['gain', 50],
     "Get out of Jail Free": ['gojf', 0],
-    # "Go to Jail. Go directly to jail, do not pass Go, do not collect $200": ['jail', 0],
-    # "Holiday fund matures. Receive $100": ['gain', 100],
-    # "Income tax refund. Collect $20": ['gain', 20],
+    "Go to Jail. Go directly to jail, do not pass Go, do not collect $200": ['jail', 0],
+    "Holiday fund matures. Receive $100": ['gain', 100],
+    "Income tax refund. Collect $20": ['gain', 20],
     "It is your birthday. Collect $10 from every player": ['bday', 10],
-    # "Life insurance matures. Collect $100": ['gain', 100],
-    # "Pay hospital fees of $100": ['loss', 100],
-    # "Pay school fees of $50": ['loss', 50],
-    # "Receive $25 consultancy fee": ['loss', 25],
+    "Life insurance matures. Collect $100": ['gain', 100],
+    "Pay hospital fees of $100": ['loss', 100],
+    "Pay school fees of $50": ['loss', 50],
+    "Receive $25 consultancy fee": ['loss', 25],
     "You are assessed for street repairs: $40 per house, $115 per hotel": ['repair', 'cc'],
-    # "You have won second prize in a beauty contest. Collect $10": ['gain', 10],
-    # "You inherit $100": ['gain', 100],
+    "You have won second prize in a beauty contest. Collect $10": ['gain', 10],
+    "You inherit $100": ['gain', 100],
 }
 CC_GOJF_LOCK = False
 
